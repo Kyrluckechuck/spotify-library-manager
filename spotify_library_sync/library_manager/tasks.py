65f84a862248804116f34125be729ad6bcbe2443
Justin Kyryliuk <justin@kyryli.uk>
2024-02-24 14:42:29 -0500

    Optimize Priorities

--- spotify_library_sync/library_manager/tasks.py
+++ spotify_library_sync/library_manager/tasks.py
@@ -9,7 +9,7 @@ from huey_monitor.tqdm import ProcessInfo
 
 from django.db.models.functions import Now
 
-@huey.task()
+@huey.task(priority=3)
 def fetch_all_albums_for_artist(artist_id: int):
     artist = Artist.objects.get(id=artist_id)
     downloader_config = Config()
@@ -17,7 +17,7 @@ def fetch_all_albums_for_artist(artist_id: int):
     downloader_config.urls = []
     downloader_main(downloader_config)
 
-@huey.task(context=True)
+@huey.task(context=True, priority=1)
 def download_missing_albums_for_artist(artist_id: int, task: Task = None):
     artist = Artist.objects.get(id=artist_id)
     missing_albums = Album.objects.filter(artist=artist, downloaded=False, wanted=True)
@@ -36,7 +36,7 @@ def download_missing_albums_for_artist(artist_id: int, task: Task = None):
     artist.last_synced_at = Now()
     artist.save()
 
-@huey.task(context=True)
+@huey.task(context=True, priority=2)
 def download_playlist(playlist_url: str, tracked: bool = True, task: Task = None):
     downloader_config = Config()
     downloader_config.urls = [playlist_url]
@@ -47,7 +47,7 @@ def download_playlist(playlist_url: str, tracked: bool = True, task: Task = None
     downloader_main(downloader_config)
 
 # Disable period tasks for now
-@huey.periodic_task(crontab(minute='0', hour='*/2'))
+@huey.periodic_task(crontab(minute='0', hour='*/2'), priority=1)
 # @huey.task(priority=10)
 def update_tracked_artists():
     all_tracked_artists = Artist.objects.filter(tracked=True).order_by("last_synced_at", "added_at", "id")
@@ -63,7 +63,7 @@ def download_missing_tracked_artists():
     already_enqueued_artists = helpers.convert_first_task_args_to_list(existing_tasks)
     helpers.download_missing_tracked_artists(already_enqueued_artists, all_tracked_artists)
 
-@huey.periodic_task(crontab(minute='0', hour='*/6'))
+@huey.periodic_task(crontab(minute='0', hour='*/6'), priority=1)
 # @huey.task(priority=10)
 def sync_tracked_playlists():
     all_enabled_playlists = TrackedPlaylist.objects.filter(enabled=True).order_by("last_synced_at", "id")
